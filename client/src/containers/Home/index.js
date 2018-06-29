415b33e20aa599d1fae6a5f5c156548424b6381d
Amine Benselim <amine.benselim@friday.de>
2018-06-29 16:25:46 +0200

    tweak Home component

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
 
-const Home = () => <React.Fragment />;
+import InputField from '../../components/InputField';
+import { createLink } from '../../links/reducer';
+import styles from './home.scss';
 
-export default connect()(Home);
+const Home = ({ generateHash }) => (
+  <div className={styles.homeWrapper}>
+    <InputField generateHash={generateHash} />
+  </div>
+);
+
+const mapDispatchToProps = dispatch => ({
+  generateHash: url => dispatch(createLink.request({ url })),
+});
+
+export default connect(
+  undefined,
+  mapDispatchToProps,
+)(Home);
